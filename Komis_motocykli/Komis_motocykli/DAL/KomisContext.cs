c512906dbe2ef601ff190dbc2cf806a96d140278
DESKTOP-LSSU7L6\marek <maruw4k@gmail.com>
2017-03-24 22:48:41 +0100

    dodanie KursyContext

---
+++ Komis_motocykli/Komis_motocykli/DAL/KomisContext.cs
@@ -0,0 +1,33 @@
+﻿using Komis_motocykli.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Komis_motocykli.DAL
+{
+    public class KomisContext : DbContext
+    {
+        public KomisContext() : base("KomisContext")
+        {
+             
+        }
+
+        public DbSet<Motocykl> Motocykle { get; set; }
+        public DbSet<Kategoria> Kategoria { get; set; }
+        public DbSet<Zamowienie> Zamowienie { get; set; }
+
+       
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // zmiana konwencji nazywania (bez koncowek s)
+            // zamiast Kategorie zostałaby stworzona tabela o nazwie Kategories
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
