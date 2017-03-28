c173c3c193d775430f784fecfa21bca6107f7bc1
DESKTOP-LSSU7L6\marek <maruw4k@gmail.com>
2017-03-29 00:40:17 +0200

    Dodanie widoku logowania i rejestracji

---
+++ Komis_motocykli/Komis_motocykli/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Komis_motocykli.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Komis_motocykli.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModels model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+                return View(model);
+            else
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Komis_motocykli/Komis_motocykli/ViewModels/AccountViewModels.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Komis_motocykli.ViewModels
+{
+    public class LoginViewModels
+    {
+        [Required(ErrorMessage = "Musisz wprowadzić adres email")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Musis wprowadzić hasło")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnnie")]
+        public bool RememberMe { get; set; }
+    }
+
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "{0} musi mieć co najmniej {2} znaków.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = " Hasło ")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdz Hasło ")]
+        [Compare("Password", ErrorMessage = "Hasło i potwierdzenie hasła nie pasują do siebie.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+
+}
